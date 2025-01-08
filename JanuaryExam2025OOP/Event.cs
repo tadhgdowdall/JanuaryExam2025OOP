b6d03a87d3b659490b28e5e744d9a4d12e22def5
Tadhg Dowdall <S00250843@atu.ie>
2025-01-08 13:31:20 +0000

    Event Class

---
+++ JanuaryExam2025OOP/Event.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanuaryExam2025OOP
+{
+    // Event type with set values 
+    public enum EventType { Music, Comedy, Theatre}
+
+    internal class Event
+    {
+
+        #region Properties
+        //Properties 
+        public string  Name { get; set; }
+        public DateTime EventDate { get; set; }
+        public List<Ticket> Tickets{ get; set; }
+        public EventType TypeOfEvent { get; set; }
+        #endregion
+
+        #region Constructors
+        public Event() { }
+
+        public Event(string name, DateTime eventDate, List<Ticket> tickets, EventType typeOfEvent)
+        {
+            Name = name;
+            EventDate = eventDate;
+            Tickets = tickets;
+            TypeOfEvent = typeOfEvent;
+        }
+        #endregion
+
+    }
+}
