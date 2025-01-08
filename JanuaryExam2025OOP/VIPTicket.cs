f76c9767310e5622deaa2909f58760acfe4cab82
Tadhg Dowdall <S00250843@atu.ie>
2025-01-08 13:27:46 +0000

    Created VIP Ticket Class

--- JanuaryExam2025OOP/VIPTicket.cs
+++ JanuaryExam2025OOP/VIPTicket.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace JanuaryExam2025OOP
 {
-    internal class VIPTicket
+    internal class VIPTicket : Ticket
     {
+        #region Properties
+        //properties
+        public string AdditionalExtras { get; set; }
+        public decimal AdditionalCost { get; set; }
+        #endregion
+
+
+        #region Constructors
+        // Constructor inherits values from the base ticket 
+
+        public VIPTicket() { }
+        public VIPTicket(string name, decimal price, int availableTickets, string AdditionalExtras, decimal AdditionalCost) : base(name, price, availableTickets) { }
+        #endregion
     }
 }
