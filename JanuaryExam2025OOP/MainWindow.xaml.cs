39d6559c7d80bb3d5a1c0d11f88568cb6b459922
Tadhg Dowdall <S00250843@atu.ie>
2025-01-08 13:23:55 +0000

    Created the ticket class

--- JanuaryExam2025OOP/MainWindow.xaml.cs
+++ JanuaryExam2025OOP/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/*https://github.com/tadhgdowdall/JanuaryExam2025OOP */
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
---
+++ JanuaryExam2025OOP/Ticket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanuaryExam2025OOP
+{
+
+    internal class Ticket
+    {
+        #region Properties
+        //properties
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int AvailableTickets { get; set; }
+        #endregion
+
+        #region Constructors 
+        //constructors
+        public Ticket() { }
+
+        public Ticket (string name, decimal price, int availableTickets)
+        {
+            Name = name;
+            Price = price;
+            AvailableTickets = availableTickets;
+        }   
+
+        public Ticket(string name, decimal price) : this(name, price, 0) { }
+        #endregion
+
+
+    }
+}
